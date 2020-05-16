d639e6c189d97090695ce3b44ce7ddd9eb7f9e9c
narise378 <narise378@gmail.com>
2020-05-16 12:28:31 +0900

    データーバインディング、データの受け渡し

--- components/Posts.vue
+++ components/Posts.vue
@@ -1,18 +1,31 @@
 <template>
  <div>
    <div class="posts overflow-scroll mb-24">
-     <post />
-     <post />
+     <post :post="post" :user="user" :be-liked="beLiked" />
+     <post :post="post" :user="user" />
    </div>
  </div>
 </template>
 
 <script>
-import Post from '~/components/Post'
+import Post from '~/components/Post.vue'
 
 export default {
-    components: {
-        Post
+  components: {
+    Post
+  },
+  data () {
+    return {
+      user: {
+        displayName: 'nari369',
+        photoURL: '/images/post0.jpg',
+      },
+      post: {
+        text: 'NOW TOYAMA',
+        image: 'images/post1.jpg'
+      },
+      beLiked: true
     }
+  }
 }
 </script>
